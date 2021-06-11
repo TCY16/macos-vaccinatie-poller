3d94654f595099c8766db8d21ce387cf9fcce706
Tom Carpay <tom@nlnetlabs.nl>
2021-06-11 12:52:39 +0200

    fix infinite loop without result

--- magikalvaccinatie.py
+++ magikalvaccinatie.py
@@ -1,15 +1,18 @@
 import os
+import time
 
 year = "1990"
 path = "~/" # Path to the location of the greensquare.png
 
 query = "curl https://user-api.coronatest.nl/vaccinatie/programma/bepaalbaar/"+ year +"/NEE/NEE"
 
-response = os.popen(query).read()
-
 print("watching...")
 while(True):
+	response = os.popen(query).read()
+
 	if "true" in response:
 		print("JE MAG EEN VACCINATIE!")
 		os.system("open "+ path +"greensquare.png")
 		break
+
+	time.sleep(30)
